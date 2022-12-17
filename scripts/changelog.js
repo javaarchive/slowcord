00673a3466098eb9f9712fd626aee4d56c2a32ec
Raymond <20248577+javaarchive@users.noreply.github.com>
2022-12-17 11:52:02 -0800

    cache clean

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -2,9 +2,15 @@ const fetch = require("node-fetch");
 const fs = require("fs/promises");
 const path = require("path");
 
+// Currently we don't connect to the database in this script so configuration specifacation right now is done via env or .env file
+// env vars are prefixed with SLOWCORD to avoid conflicts
+require("dotenv").config({
+	path: path.join(__dirname, "..", ".env"), // use .env in root directory
+}); 
+
 const CACHE_PATH = path.join(__dirname, "..", "assets", "cache");
 const CHANGELOG_PATH = path.join(__dirname, "..", "assets", "changelog.txt");
-const BASE_URL = "https://discord.com";
+const BASE_URL = process.env.SLOWCORD_BASE_URL || "https://discord.com";
 
 const CHANGELOG_SCRIPT = "9c4b2d313c6e1c864e89.js";
 
